0133d2c6976ed2c53321be253e495b12150ee55d
Nikolay <nikolai-vyalkov@yandex.ru>
2020-07-30 10:28:01 +0500

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,13 @@
+public class BonusMilesService{
+    public int calculate(int cost) {
+        int mile = 20;
+        int bonus = cost/mile;
+        System.out.println(bonus);
+        return mile;
+
+            }
+
+}
+
+
+
