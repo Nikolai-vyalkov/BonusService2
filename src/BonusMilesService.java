db306e0309e3dad1806ba36536cab8c4721896c2
Nikolay <nikolai-vyalkov@yandex.ru>
2020-08-04 18:21:55 +0500

    BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,10 @@
-public class BonusMilesService{
-    public int calculate(int cost,int mile) {
-        int bonus = cost/mile;
+public class BonusMilesService {
+    public int calculate(int cost, int mile) {
+        int bonus = cost / mile;
 
         return bonus;
 
-            }
+    }
 
 }
 
